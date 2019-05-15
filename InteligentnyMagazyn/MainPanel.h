0d9939e7972afeff2912d40e0319700e5ea7637b
kpasierbiewicz <kpasierbiewicz.programista@gmail.com>
2019-05-15 21:24:51 +0200

    Panel z przejściami

--- InteligentnyMagazyn/MainPanel.h
+++ InteligentnyMagazyn/MainPanel.h
@@ -38,6 +38,9 @@ namespace InteligentnyMagazyn {
 	private: System::Windows::Forms::Button^  databaseToolsButton;
 	private: System::Windows::Forms::Button^  databseBigElementsButton;
 	private: System::Windows::Forms::Button^  databasesDelivererButton;
+	private: System::Windows::Forms::Button^  searchButton;
+	private: System::Windows::Forms::Button^  editDatabaseButton;
+
 	protected:
 
 	private:
@@ -58,6 +61,8 @@ namespace InteligentnyMagazyn {
 			this->databaseToolsButton = (gcnew System::Windows::Forms::Button());
 			this->databseBigElementsButton = (gcnew System::Windows::Forms::Button());
 			this->databasesDelivererButton = (gcnew System::Windows::Forms::Button());
+			this->searchButton = (gcnew System::Windows::Forms::Button());
+			this->editDatabaseButton = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// databaseElementsButton
@@ -121,13 +126,51 @@ namespace InteligentnyMagazyn {
 			this->databasesDelivererButton->UseVisualStyleBackColor = false;
 			this->databasesDelivererButton->Click += gcnew System::EventHandler(this, &MainPanel::databasesDelivererButton_Click);
 			// 
+			// searchButton
+			// 
+			this->searchButton->BackColor = System::Drawing::Color::BurlyWood;
+			this->searchButton->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->searchButton->Font = (gcnew System::Drawing::Font(L"Arial Black", 16.2F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->searchButton->ForeColor = System::Drawing::Color::LimeGreen;
+			this->searchButton->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"searchButton.Image")));
+			this->searchButton->Location = System::Drawing::Point(702, 213);
+			this->searchButton->Name = L"searchButton";
+			this->searchButton->Size = System::Drawing::Size(1061, 48);
+			this->searchButton->TabIndex = 4;
+			this->searchButton->Tag = L"N";
+			this->searchButton->Text = L"Szukaj";
+			this->searchButton->UseVisualStyleBackColor = false;
+			this->searchButton->Visible = false;
+			this->searchButton->Click += gcnew System::EventHandler(this, &MainPanel::searchButton_Click);
+			// 
+			// editDatabaseButton
+			// 
+			this->editDatabaseButton->BackColor = System::Drawing::Color::BurlyWood;
+			this->editDatabaseButton->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->editDatabaseButton->Font = (gcnew System::Drawing::Font(L"Arial Black", 16.2F, static_cast<System::Drawing::FontStyle>((System::Drawing::FontStyle::Bold | System::Drawing::FontStyle::Italic)),
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->editDatabaseButton->ForeColor = System::Drawing::Color::LimeGreen;
+			this->editDatabaseButton->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"editDatabaseButton.Image")));
+			this->editDatabaseButton->Location = System::Drawing::Point(702, 124);
+			this->editDatabaseButton->Name = L"editDatabaseButton";
+			this->editDatabaseButton->Size = System::Drawing::Size(1061, 48);
+			this->editDatabaseButton->TabIndex = 5;
+			this->editDatabaseButton->Tag = L"N";
+			this->editDatabaseButton->Text = L"Edytuj Baze";
+			this->editDatabaseButton->UseVisualStyleBackColor = false;
+			this->editDatabaseButton->Visible = false;
+			this->editDatabaseButton->Click += gcnew System::EventHandler(this, &MainPanel::editDatabaseButton_Click);
+			// 
 			// MainPanel
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(120, 120);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Dpi;
 			this->AutoSize = true;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
-			this->ClientSize = System::Drawing::Size(838, 850);
+			this->ClientSize = System::Drawing::Size(1710, 850);
+			this->Controls->Add(this->editDatabaseButton);
+			this->Controls->Add(this->searchButton);
 			this->Controls->Add(this->databasesDelivererButton);
 			this->Controls->Add(this->databseBigElementsButton);
 			this->Controls->Add(this->databaseToolsButton);
@@ -142,6 +185,19 @@ namespace InteligentnyMagazyn {
 
 		}
 #pragma endregion
+
+	private:
+		bool databaseElementsButtonClick = false;
+		bool databaseToolsButtonClick = false;
+		bool databseBigElementsButtonClick = false;
+		bool databasesDelivererButtonClick = false;
+		bool editDatabaseButtonClick = false;
+		bool searchButtonClick = false;
+	
+		private: void firstStanWhenStartProgram() {
+			editDatabaseButtonClick = false;
+			searchButtonClick = true;
+		}
 	private: System::Void MainPanel_FormClosed(System::Object^  sender, System::Windows::Forms::FormClosedEventArgs^  e) {
 		if (MessageBox::Show("Zamkniêcie aplikacji", "Czy na pewno chcesz zakoñczyæ?",
 			MessageBoxButtons::YesNo, MessageBoxIcon::Question) == System::Windows::Forms::DialogResult::Yes) {
@@ -154,15 +210,69 @@ namespace InteligentnyMagazyn {
 	}
 	private: System::Void databaseElementsButton_Click(System::Object^  sender, System::EventArgs^  e) {
 		this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno2-1.jpg");
+		this->editDatabaseButton->Visible = true;
+		this->searchButton->Visible = true;
+
+		databaseElementsButtonClick = true;
+		databaseToolsButtonClick = false;
+		databseBigElementsButtonClick = false;
+		databasesDelivererButtonClick = false;
+
+		firstStanWhenStartProgram();
 	}
 	private: System::Void databaseToolsButton_Click(System::Object^  sender, System::EventArgs^  e) {
 		this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno3-1.jpg");
+		this->editDatabaseButton->Visible = true;
+		this->searchButton->Visible = true;
+
+		databaseElementsButtonClick = false;
+		databaseToolsButtonClick = true;
+		databseBigElementsButtonClick = false;
+		databasesDelivererButtonClick = false;
+
+		firstStanWhenStartProgram();
 	}
 	private: System::Void databseBigElementsButton_Click(System::Object^  sender, System::EventArgs^  e) {
 		this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno4-1.jpg");
+		this->editDatabaseButton->Visible = true;
+		this->searchButton->Visible = true;
+
+		databaseElementsButtonClick = false;
+		databaseToolsButtonClick = false;
+		databseBigElementsButtonClick = true;
+		databasesDelivererButtonClick = false;
+
+		firstStanWhenStartProgram();
 	}
 	private: System::Void databasesDelivererButton_Click(System::Object^  sender, System::EventArgs^  e) {
 		this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno5-1.jpg");
+		this->editDatabaseButton->Visible = true;
+		this->searchButton->Visible = true;
+
+		databaseElementsButtonClick = false;
+		databaseToolsButtonClick = false;
+		databseBigElementsButtonClick = false;
+		databasesDelivererButtonClick = true;
+
+		firstStanWhenStartProgram();
 	}
-};
+	private: System::Void editDatabaseButton_Click(System::Object^  sender, System::EventArgs^  e) {
+		editDatabaseButtonClick = true;
+		searchButtonClick = false;
+
+		if (databaseElementsButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno2-2.jpg");
+		else if (databaseToolsButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno3-2.jpg");
+		else if (databseBigElementsButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno4-2.jpg");
+		else if (databasesDelivererButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno5-2.jpg");
+	}
+	private: System::Void searchButton_Click(System::Object^  sender, System::EventArgs^  e) {
+		editDatabaseButtonClick = false;
+		searchButtonClick = true;
+
+		if (databaseElementsButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno2-1.jpg");
+		else if (databaseToolsButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno3-1.jpg");
+		else if (databseBigElementsButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno4-1.jpg");
+		else if (databasesDelivererButtonClick) this->BackgroundImage = Image::FromFile("images\\imgPanels\\okno5-1.jpg");
+	}
+	};
 }
